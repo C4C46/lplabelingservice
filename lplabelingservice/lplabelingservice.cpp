6044d625f31663cd16ddbe782e28c93e0dce0fa6
cao.peixuan <cao.peixuan@hzleaper.com>
2024-05-28 15:51:46 +0800

    贴标逻辑下位机通信

--- lplabelingservice/lplabelingservice.cpp
+++ lplabelingservice/lplabelingservice.cpp
@@ -1,5 +1,139 @@
-#include "lplabelingservice.h"
+﻿#include "lplabelingservice.h"
 
-lplabelingservice::lplabelingservice()
+#include "lpservicestatusmsg.h"
+#include "lpwavedatamsg.h"
+#include <QTimer>
+lplabelingservice* g_instance = nullptr;
+
+lplabelingservice::lplabelingservice(QString strName)
+	:LPMQService(strName),
+	d(new LPLabelingServicePrivate)
 {
+	d->heart_timer_ptr_ = QSharedPointer<LPTaskTimer>(new LPTaskTimer());
+	d->heart_timer_ptr_->registTask("heart", std::bind(&lplabelingservice::sendHeartInfo, this));
+	g_instance = this;
+	d->heart_timer_ptr_->start();
+}
+
+void lplabelingservice::sendHeartInfo()
+{
+	//QMutexLocker locker(&m_mutex);
+	lpbs::CameraWaveStatusInfo heartInfo;
+	heartInfo.serviceName = LPMQService::name();
+	QByteArray ba;
+	QDataStream out(&ba, QIODevice::WriteOnly);
+	out << heartInfo;
+	sendMsg(LP_CAM_WAVE_STATUS_INFO, ba);
+
+}
+
+void lplabelingservice::recvMsg(QByteArray & ba, const MsgReceiverInfo & receiverInfo)
+{
+	QDataStream in(ba);
+	int type;
+	in >> type;
+	switch (type)
+	{
+	case LP_IPUMSG_PUB_WAVE_DATA:
+	{
+		handleCameraWaveData(in, receiverInfo);
+		break;
+	}
+	case LP_IPUMSG_PUB_STATUS_INFO:
+	{
+		handleIpuStatus(in, receiverInfo);
+		break;
+	}
+	case LP_LITHIUM_LABELING_TAG:
+	{
+		handleLabelingTag(in, receiverInfo);
+		break;
+	}
+
+	default:
+		break;
+	}
+}
+
+void lplabelingservice::handleLabelingTag(QDataStream & ds, const MsgReceiverInfo & receiverInfo)
+{
+	Lithium_LabelingTag tagInfo;
+	ds >> tagInfo;
+	// 处理接收到的标签信息
+
+	// 打印接收到的标签信息
+	qDebug() << "Received Lithium_LabelingTag:";
+	qDebug() << "  unitName:" << tagInfo.unitName;
+	qDebug() << "  centYPosMm:" << tagInfo.centYPosMm;
+	qDebug() << "  centXPosMm:" << tagInfo.centXPosMm;
+	qDebug() << "  channelId:" << tagInfo.channelId;
+	qDebug() << "  checkType:" << tagInfo.checkType;
+
+
+	forwardLabelingTag(tagInfo);
+	qDebug() << "yes " << __FUNCTION__;
+}
+
+
+void lplabelingservice::forwardLabelingTag(const Lithium_LabelingTag & tagInfo)
+{
+	// 转发逻辑
+	QByteArray ba;
+	QDataStream out(&ba, QIODevice::WriteOnly);
+	out << tagInfo;
+	sendMsg(LP_LITHIUM_LABELING_TAG, ba); // 假设下位机使用相同的消息类型
+	qDebug() << "yes " << __FUNCTION__;
+}
+
+void lplabelingservice::handleCameraWaveData(QDataStream & ds, const MsgReceiverInfo & receiverInfo)
+{
+	lpbs::WaveData info;
+	ds >> info;
+
+	if (m_camWavePeriod.contains(info.camId) && (QDateTime::currentMSecsSinceEpoch() - m_camWavePeriod[info.camId] > 1000))
+	{
+		m_camWavePeriod.insert(info.camId, QDateTime::currentMSecsSinceEpoch());
+	}
+	else
+	{
+		if (m_camWavePeriod.contains(info.camId))
+			return;
+		m_camWavePeriod.insert(info.camId, QDateTime::currentMSecsSinceEpoch());
+	}
+
+	lpbs::CameraWaveForwarding camWaveForwardingInfo;
+	camWaveForwardingInfo.data = info;
+	QByteArray ba;
+	QDataStream out(&ba, QIODevice::WriteOnly);
+	out << camWaveForwardingInfo;
+	sendMsg(LP_CAM_WAVE_FORWARDING, ba);
+}
+
+void lplabelingservice::handleIpuStatus(QDataStream & ds, const MsgReceiverInfo & receiverInfo)
+{
+	lpbs::IpuStatusInfo info;
+	ds >> info;
+	if (m_camStatusPeriod.contains(info.serviceName) && (QDateTime::currentMSecsSinceEpoch() - m_camStatusPeriod[info.serviceName] > 1000))
+	{
+		m_camStatusPeriod.insert(info.serviceName, QDateTime::currentMSecsSinceEpoch());
+	}
+	else
+	{
+		if (m_camStatusPeriod.contains(info.serviceName))
+			return;
+		m_camStatusPeriod.insert(info.serviceName, QDateTime::currentMSecsSinceEpoch());
+	}
+
+	lpbs::IpuStatusForwardingInfo cameraWaveStatusInfo;
+	cameraWaveStatusInfo.ipuStatusInfo = info;
+	QByteArray ba;
+	QDataStream out(&ba, QIODevice::WriteOnly);
+	out << cameraWaveStatusInfo;
+	sendMsg(LP_IPU_STATUS_FORWARDING_INFO, ba);
+}
+
+
+LPMQService* LpMQServiceNewInstance(const QString& name)
+{
+	return new lplabelingservice(name);
 }
--- lplabelingservice/lplabelingservice.h
+++ lplabelingservice/lplabelingservice.h
@@ -1,9 +1,50 @@
-#pragma once
+﻿#pragma once
 
-#include "lplabelingservice_global.h"
+#include "lpmqservice.h"
+#include "LPLabelingServicePrivate.h"
+#include <QString>
 
-class LPLABELINGSERVICE_EXPORT lplabelingservice
+// 新的消息类型
+enum {
+	LP_LITHIUM_LABELING_TAG = 1001, // 假设消息类型编号
+};
+
+struct Lithium_LabelingTag {
+	QString unitName;
+	qreal centYPosMm;
+	qreal centXPosMm;
+	int channelId;
+	QString checkType;
+};
+
+// QDataStream 序列化操作符
+inline QDataStream &operator<<(QDataStream &out, const Lithium_LabelingTag &tag) {
+	out << tag.unitName << tag.centYPosMm << tag.centXPosMm << tag.channelId << tag.checkType;
+	return out;
+}
+
+// QDataStream 反序列化操作符
+inline QDataStream &operator>>(QDataStream &in, Lithium_LabelingTag &tag) {
+	in >> tag.unitName >> tag.centYPosMm >> tag.centXPosMm >> tag.channelId >> tag.checkType;
+	return in;
+}
+
+
+class LPLabelingServicePrivate;
+class LPMQSERVICE_API lplabelingservice : public LPMQService
 {
 public:
-    lplabelingservice();
+	lplabelingservice(QString strName);
+	void sendHeartInfo();
+	virtual void recvMsg(QByteArray& ba, const MsgReceiverInfo& receiverInfo);
+private:
+	void handleCameraWaveData(QDataStream& ds, const MsgReceiverInfo& receiverInfo);
+	void handleIpuStatus(QDataStream& ds, const MsgReceiverInfo& receiverInfo);
+	void handleLabelingTag(QDataStream & ds, const MsgReceiverInfo & receiverInfo);
+	void forwardLabelingTag(const Lithium_LabelingTag & tagInfo);
+private:
+	QScopedPointer<LPLabelingServicePrivate> d;
+	QMutex m_mutex;
+	QMap<int, qint64> m_camWavePeriod;
+	QMap<QString, qint64> m_camStatusPeriod;
 };
